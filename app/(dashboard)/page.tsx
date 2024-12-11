8ea616b4cd824cf587edfd4d2824b91247f137ef
rayan <r.lazghab21167@pi.tn>
2024-12-11 18:43:06 +0100

    sign out btn  update

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,11 @@
-import { UserButton } from "@clerk/nextjs"
+import { SignOutButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <UserButton afterSignOutUrl="/" />
-    )
+    <SignOutButton>
+      <button className="px-4 py-2 bg-red-500 text-white font-semibold rounded-md hover:bg-red-600 transition">
+        Sign Out
+      </button>
+    </SignOutButton>
+  );
 }
